d0dfa60675d318e807ed857608198061f2f12cba
N <thenykreed@gmail.com>
2020-02-01 17:57:01 -0600

    YO

--- Scenes/NewLevels/Reactants/Tree.gd
+++ Scenes/NewLevels/Reactants/Tree.gd
@@ -1,15 +1,18 @@
 extends "res://Scenes/NewLevels/SimulatedObject/Base.gd"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var alive : bool = true
+
 
 func _body_entered(body: Node) -> void:
 	if body.is_in_group("simulated"):
 		set("mode", RigidBody.MODE_RIGID)
+		alive = false
+		$Particles.emitting = true
 
 func stop() -> void:
 	set("mode", RigidBody.MODE_STATIC)
 
 func reset() -> void:
+	$Particles.emitting = false
 	transform = initial_transform
+	alive = true
